5e3ab5f16b749b5d3ca919860f004cef2264c08e
shonabotbr32 <shonabots@gmail.com>
2026-05-28 01:45:49 +0530

    Update speed.py

--- SONALI/utils/inline/speed.py
+++ SONALI/utils/inline/speed.py
@@ -1,5 +1,4 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.enums import ButtonStyle
 
 
 def speed_markup(_, chat_id):
@@ -9,41 +8,34 @@ def speed_markup(_, chat_id):
                 InlineKeyboardButton(
                     text="🕒 0.5x",
                     callback_data=f"SpeedUP {chat_id}|0.5",
-                    style=ButtonStyle.SECONDARY,
                 ),
                 InlineKeyboardButton(
                     text="🕓 0.75x",
                     callback_data=f"SpeedUP {chat_id}|0.75",
-                    style=ButtonStyle.PRIMARY,
                 ),
             ],
             [
                 InlineKeyboardButton(
                     text=_["P_B_4"],
                     callback_data=f"SpeedUP {chat_id}|1.0",
-                    style=ButtonStyle.SUCCESS,
                 ),
             ],
             [
                 InlineKeyboardButton(
                     text="🕤 1.5x",
                     callback_data=f"SpeedUP {chat_id}|1.5",
-                    style=ButtonStyle.PRIMARY,
                 ),
                 InlineKeyboardButton(
                     text="🕛 2.0x",
                     callback_data=f"SpeedUP {chat_id}|2.0",
-                    style=ButtonStyle.DANGER,
                 ),
             ],
             [
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
                     callback_data="close",
-                    style=ButtonStyle.DANGER,
                 ),
             ],
         ]
     )
-
     return upl
