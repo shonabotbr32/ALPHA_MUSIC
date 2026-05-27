ffb2451546e89594fb38af5e3ce31e3e94603986
shonabotbr32 <shonabots@gmail.com>
2026-05-27 21:13:37 +0530

    Update start.py

--- SONALI/utils/inline/start.py
+++ SONALI/utils/inline/start.py
@@ -1,4 +1,5 @@
 from pyrogram.types import InlineKeyboardButton
+from pyrogram.enums import ButtonStyle
 
 import config
 from SONALI import app
@@ -8,11 +9,18 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+                style=ButtonStyle.PRIMARY,
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text=_["S_B_2"],
+                url=config.SUPPORT_CHAT,
+                style=ButtonStyle.SUCCESS,
+            ),
         ],
     ]
+
     return buttons
 
 
@@ -22,17 +30,28 @@ def private_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
+                style=ButtonStyle.PRIMARY,
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text=_["S_B_5"],
+                user_id=config.OWNER_ID,
+                style=ButtonStyle.SECONDARY,
+            ),
+            InlineKeyboardButton(
+                text=_["S_B_2"],
+                url=config.SUPPORT_CHAT,
+                style=ButtonStyle.SUCCESS,
+            ),
         ],
         [
             InlineKeyboardButton(
                 text=_["S_B_4"],
-                callback_data="settings_back_helper"
+                callback_data="settings_back_helper",
+                style=ButtonStyle.DANGER,
             )
         ],
     ]
+
     return buttons
