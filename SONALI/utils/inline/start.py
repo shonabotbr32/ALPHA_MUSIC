2114928cdab9860862cd758bcd7e57580ba77a6f
shonabotbr32 <shonabots@gmail.com>
2026-05-27 23:36:44 +0530

    Update start.py

--- SONALI/utils/inline/start.py
+++ SONALI/utils/inline/start.py
@@ -11,12 +11,12 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-                style=ButtonStyle.PRIMARY,
+                style=ButtonStyle.primary,
             ),
             InlineKeyboardButton(
                 text=_["S_B_2"],
                 url=config.SUPPORT_CHAT,
-                style=ButtonStyle.SUCCESS,
+                style=ButtonStyle.success,
             ),
         ],
     ]
@@ -30,26 +30,26 @@ def private_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-                style=ButtonStyle.PRIMARY,
+                style=ButtonStyle.primary,
             )
         ],
         [
             InlineKeyboardButton(
                 text=_["S_B_5"],
                 user_id=config.OWNER_ID,
-                style=ButtonStyle.SECONDARY,
+                style=ButtonStyle.secondary,
             ),
             InlineKeyboardButton(
                 text=_["S_B_2"],
                 url=config.SUPPORT_CHAT,
-                style=ButtonStyle.SUCCESS,
+                style=ButtonStyle.success,
             ),
         ],
         [
             InlineKeyboardButton(
                 text=_["S_B_4"],
                 callback_data="settings_back_helper",
-                style=ButtonStyle.DANGER,
+                style=ButtonStyle.danger,
             )
         ],
     ]
